a3475eb70be25eea70d5d523ca11510d3256eb9e
John Deng <john.deng@qq.com>
2025-12-02 12:12:38 +0800

    added token verification

--- pkg/oidc/autoconfigure.go
+++ pkg/oidc/autoconfigure.go
@@ -1,11 +1,13 @@
 package oidc
 
 import (
+	"context"
 	"strings"
 	"time"
 
+	"github.com/coreos/go-oidc/v3/oidc"
 	"github.com/hidevopsio/hiboot/pkg/app"
-	"github.com/hidevopsio/hiboot/pkg/app/web/context"
+	hcontext "github.com/hidevopsio/hiboot/pkg/app/web/context"
 	"github.com/hidevopsio/hiboot/pkg/at"
 	"github.com/hidevopsio/hiboot/pkg/log"
 	"k8s.io/apimachinery/pkg/api/errors"
@@ -17,10 +19,14 @@ const (
 
 type configuration struct {
 	at.AutoConfiguration
+
+	prop *Properties
 }
 
-func newConfiguration() *configuration {
-	return &configuration{}
+func newConfiguration(prop *Properties) *configuration {
+	return &configuration{
+		prop: prop,
+	}
 }
 
 func init() {
@@ -31,13 +37,13 @@ func init() {
 type Token struct {
 	at.ContextAware
 
-	Context context.Context `json:"context"`
-	Data    string          `json:"data"`
-	Claims  *Claims         `json:"claims"`
+	Context hcontext.Context `json:"context"`
+	Data    string           `json:"data"`
+	Claims  *Claims          `json:"claims"`
 }
 
 // Token instantiate bearer token to object
-func (c *configuration) Token(ctx context.Context) (token *Token, err error) {
+func (c *configuration) Token(ctx hcontext.Context) (token *Token, err error) {
 	token = new(Token)
 	if ctx == nil {
 		err = errors.NewBadRequest("unknown context")
@@ -54,11 +60,28 @@ func (c *configuration) Token(ctx context.Context) (token *Token, err error) {
 		pe := err
 		err = errors.NewUnauthorized("Unauthorized")
 		log.Errorf("%v -> %v", pe, err)
-		return  // fixes the nil pointer issue
+		return // fixes the nil pointer issue
 	}
 	if token.Claims.Expiry.Before(time.Now()) {
 		err = errors.NewUnauthorized("Expired")
 		log.Errorf("%v", err)
 	}
+
+	if c.prop.Verify {
+		provider, err := oidc.NewProvider(context.TODO(), c.prop.Issuer)
+		if err != nil {
+			log.Error(err)
+			return nil, err
+		}
+
+		verifier := provider.Verifier(&oidc.Config{ClientID: c.prop.ClientID})
+
+		_, err = verifier.Verify(context.TODO(), token.Data)
+		if err != nil {
+			log.Error(err)
+			return nil, err
+		}
+	}
+
 	return
 }
---
+++ pkg/oidc/properties.go
@@ -0,0 +1,10 @@
+package oidc
+
+import "github.com/hidevopsio/hiboot/pkg/at"
+
+type Properties struct {
+	at.ConfigurationProperties `value:"oidc"`
+	Verify   bool   `json:"verify"`
+	Issuer   string `json:"issuer"`
+	ClientID string `json:"clientId"`
+}
